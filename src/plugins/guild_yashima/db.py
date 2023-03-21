9845b8c69d64e332ee1a3c510e78937516e6f528
VonXXGhost <me@vonxxghost.xyz>
2023-03-21 23:56:17 +0800

    开发中：直播日程相关

--- src/plugins/guild_yashima/db.py
+++ src/plugins/guild_yashima/db.py
@@ -1,6 +1,8 @@
 """
 封装一些数据库的操作
 """
+import datetime
+
 from peewee import *
 
 db = DatabaseProxy()
@@ -8,10 +10,31 @@ db = DatabaseProxy()
 
 def init_database(db_path: str):
     global db
-    _db = SqliteDatabase(db_path)
-    db.initialize(db)
+    _db = SqliteDatabase(db_path, pragmas={
+        'journal_mode': 'wal',
+        'cache_size': -1 * 8000,  # 8MB
+        'ignore_check_constraints': 0,
+        'synchronous': 0})
+    db.initialize(_db)
+    _db.connect()
+    _db.create_tables(models=[StreamSchedule])
 
 
 class BaseModel(Model):
+    id = AutoField()
+
     class Meta:
         database = db
+
+
+class StreamSchedule(BaseModel):
+    """
+    直播日程
+    """
+    work_name = CharField(unique=True)  # 作品名称
+    date_schedule = CharField()  # 计划直播日期
+    time_schedule = CharField()  # 计划直播时间
+
+    update_user_id = CharField()
+    update_user_nickname = CharField()
+    update_time = DateTimeField(default=datetime.datetime.now)
